dc93e741fcaabb7d9fdbcf15f51339caba35d47d
ejjeong9103 <ejjeong9103@naver.com>
2024-12-10 09:35:25 +0900

    feat: DTO 추가

---
+++ backend/src/main/java/com/howmuch/backend/entity/dto/CityResponse.java
@@ -0,0 +1,13 @@
+package com.howmuch.backend.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class CityResponse {
+
+    private String cityName;
+
+}
---
+++ backend/src/main/java/com/howmuch/backend/entity/dto/PlanRequest.java
@@ -0,0 +1,33 @@
+package com.howmuch.backend.entity.dto;
+
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+@Data
+public class PlanRequest {
+
+    private String purpose;
+    private String period;
+    private String city;
+
+    private String days;
+
+    public void setPeriod(String period) {
+        this.period = period;
+        if (period != null && period.contains("to")) {
+            String[] dates = period.split(" to "); // "YYYY-MM-DD to YYYY-MM-DD"를 분리
+            LocalDate startDate = LocalDate.parse(dates[0], DateTimeFormatter.ISO_DATE);
+            LocalDate endDate = LocalDate.parse(dates[1], DateTimeFormatter.ISO_DATE);
+
+            long nights = ChronoUnit.DAYS.between(startDate, endDate); // 숙박일 계산
+            this.days = nights + "박 " + (nights + 1) + "일"; // "X박 X일" 형태로 설정
+        } else {
+            this.days = "기간이 잘못되었습니다."; // 예외 처리
+        }
+    }
+
+}
