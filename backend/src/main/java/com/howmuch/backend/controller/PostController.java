4c8f8cf148b6e8807bc002422ba1e38933457ae3
seohyun96 <shanan9623@gmail.com>
2024-12-13 16:26:35 +0900

    feat: 좋아요 및 조회수 순 정렬 조회, 좋아요 등록/취소 기능 추가

--- backend/src/main/java/com/howmuch/backend/controller/PostController.java
+++ backend/src/main/java/com/howmuch/backend/controller/PostController.java
@@ -1,12 +1,20 @@
 package com.howmuch.backend.controller;
 
+
+import com.howmuch.backend.entity.community.Post;
+import com.howmuch.backend.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
+@RequiredArgsConstructor
 
 public class PostController {
+
+    private final PostService postService;
 
     // 게시글 작성
     @PostMapping
@@ -36,32 +44,41 @@ public class PostController {
         return ResponseEntity.ok("조회완료");
     }
 
-    // 게시글 단건 조회
-    @GetMapping("/{postId}")
-    public ResponseEntity getPostId() {
+//    // 게시글 단건 조회
+//    @GetMapping("/{postId}")
+//    public ResponseEntity<PostResponse> getPostId(@PathVariable Long postId) {
+//        postService.incrementViewCount(postId);  // 조회수 증가
+//        PostResponse posts = postService.getPostById(postId);
+//        return ResponseEntity.ok(posts);
+//    }
 
-        return ResponseEntity.ok("조회완료");
-    }
 
-    // 게시글 좋아요 On/Off
+    // 게시글 좋아요
     @PostMapping("/{postId}/like")
-    public ResponseEntity toggleLike() {
-
-        return ResponseEntity.ok("성공적으로 처리되었습니다");
+    public ResponseEntity<String> likePost(@PathVariable Long postId, @RequestParam Long userId) {
+        postService.likePost(postId, userId);
+        return ResponseEntity.ok("좋아요 등록 완료");
     }
 
-    // 좋아요 수 인기게시물 조회
-    @GetMapping("/by-likes")
-    public ResponseEntity getPostsByLikes() {
+    //게시글 좋아요 취소
+    @DeleteMapping("/{postId}/like")
+    public ResponseEntity<String> unlikePost(@PathVariable Long postId, @RequestParam Long userId) {
+        postService.unlikePost(postId, userId);
+        return ResponseEntity.ok("좋아요 취소 완료");
+    }
 
-        return ResponseEntity.ok("조회완료");
+    // 좋아요 순 인기게시물 조회
+    @GetMapping("/by-likes")
+    public ResponseEntity<List<Post>> getAllPostsByLikeCount() {
+        List<Post> popularPosts = postService.getAllPostsByLikeCount();
+        return ResponseEntity.ok(popularPosts);
     }
 
 
     // 조회수 인기게시물 조회
     @GetMapping("/by-views")
-    public ResponseEntity getPostsByViews() {
-
-        return ResponseEntity.ok("조회완료");
+    public ResponseEntity<List<Post>> getAllPostsByViewCount() {
+        List<Post> popularPosts = postService.getAllPostsByViewCount();
+        return ResponseEntity.ok(popularPosts);
     }
 }
