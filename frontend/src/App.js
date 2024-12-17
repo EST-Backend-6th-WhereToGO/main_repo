e4eea6ec4f215b105f36ed024ce4be4d38474baa
GyuchanMoon <gyuchanm@gmail.com>
2024-12-17 10:23:50 +0900

    도시 선택 페이지 화면구성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import Home from './Home'
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3'
+import CityTest from "./CityTest";
 import './App.css';
+import CitySelectionPage from './CitySelectionPage';
 
 function App() {
   const [progress, setProgress] = useState(0); // 진행 상태 (0~100)
@@ -22,6 +24,8 @@ function App() {
           <Route path="/step1" element={<Step1 updateProgress={updateProgress} />} />
           <Route path="/step2" element={<Step2 updateProgress={updateProgress} />} />
           <Route path="/step3" element={<Step3 updateProgress={updateProgress} />} />
+          <Route path="/cities/:categoryId" element={<CitySelectionPage />} />
+          <Route path="/citiestest" element={<CityTest updateProgress={updateProgress}/>} />
         </Routes>
       </div>
     </Router>
